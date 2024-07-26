a2c0dfd46dea1b41d9deed192f3f5f71075f7959
natsuki-engr <biafrajr@gmail.com>
2024-07-26 13:21:13 +0900

    lesson-06

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div style="display: flex; gap: 20px;">
+        @foreach ($movieList as $movie)
+            <div>
+                <h3>{{ $movie->title }}</h3>
+                <img src="{{ $movie->image_url }}" style="width: 200px; height: 200px;" />
+            </div>
+        @endforeach
+    </div>
+</body>
+</html>
